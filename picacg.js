bac46cde8f718708df89a4afd1bf39fff5258eeb
loveBaihai <48119001+loveBaihai@users.noreply.github.com>
2025-03-16 15:38:51 +0800

    Update picacg.js

--- picacg.js
+++ picacg.js
@@ -7,7 +7,7 @@ class Picacg extends ComicSource {
 
     minAppVersion = "1.0.0"
 
-    url = "https://cdn.jsdelivr.net/gh/venera-app/venera-configs@main/picacg.js"
+    url = "https://lovebaihai.github.io/comic-source/picacg.js"
 
     api = "https://picaapi.picacomic.com"
 
